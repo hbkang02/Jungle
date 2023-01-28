8ce226125118507817afd36c0779ba2b6585f98d
John Kang <hbkang02@gmail.com>
2023-01-27 20:40:52 -0800

    categories add new functional added

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,39 +1,29 @@
-class Admin::ProductsController < ApplicationController
+class Admin::CategoriesController < ApplicationController
 
   def index
-    @products = Product.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
-    @product = Product.new
+    @category = Category.new
   end
 
   def create
-    @product = Product.new(product_params)
+    @category = Category.new(category_params)
 
-    if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
   end
 
-  def destroy
-    @product = Product.find params[:id]
-    @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
-  end
-
   private
 
-  def product_params
-    params.require(:product).permit(
+  def category_params
+    params.require(:category).permit(
       :name,
-      :description,
       :category_id,
-      :quantity,
-      :image,
-      :price
     )
   end
 
