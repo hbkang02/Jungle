002b68535599d73558d56f990714e9cecbdf00da
John Kang <hbkang02@gmail.com>
2023-02-03 19:05:31 -0800

    added feature for testing cypress: access homepage and checking if products are present

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,12 @@
 describe("Testing Home page", () => {
-  // beforeEach(() => {
-  //   cy.visit("");
-  // });
+
   it("Visit the home page", () => {
     cy.visit('/');
-});
+  });
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
